3a64b648eccda43a04a0cb9b08116be2fd038b94
Wesley Hutchinson <wesleyhutchinson@hotmail.com>
2016-01-24 15:36:49 +0000

    final clearout

--- app/controllers/details.js
+++ app/controllers/details.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.info = function(req, res) {
-      var ip = req.headers['x-forwarded-for'] || 
+     var ip = req.headers['x-forwarded-for'] || 
      req.connection.remoteAddress || 
      req.socket.remoteAddress ||
      req.connection.socket.remoteAddress;
